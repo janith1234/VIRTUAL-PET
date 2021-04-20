88039bddb2619e6a67f18b9ab2dfe39df159954b
janith <vajanith@gmail.com>
2021-04-20 17:24:35 +0530

    changed

--- food.js
+++ food.js
@@ -19,7 +19,7 @@ class Food {
         image(this.image, 720, 220, 70, 70);
         if (this.foodStock != 0) {
             for (var i = 0; i < this.foodStock; i++) {
-                if (i % 10 == 0) {
+                if (i % 10 === 0) {
                     x = 80; y = y + 50;
                 } image(this.image, x, y, 50, 50);
                 x = x + 30;
